d35ed11d57f7e7f7f67aa24f52f712d0eb3a7f6c
Amr AbuZant <amrabuzant@gmail.com>
2018-10-10 22:58:32 +0300

    Initial Commit

---
+++ AsalTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AsalTask
+//
+//  Created by Amr Abu Zant on 10/10/18.
+//  Copyright © 2018 Amr Abu Zant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
